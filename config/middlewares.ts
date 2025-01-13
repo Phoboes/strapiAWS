749139e55c615c3a2cf9d97f8d6fe79fa942fd39
Kane <Phoboes@live.com>
2025-01-14 10:07:44 +1100

    Init -- s3 media uploader integrated.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,37 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'shopify-proto-storage.s3.ap-southeast-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'shopify-proto-storage.s3.ap-southeast-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
